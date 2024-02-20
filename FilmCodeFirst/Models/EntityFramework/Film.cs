7ab642bcbbebb1703a4791f37ef5488216fd5ea6
Léo Christophe <Leo.Christophe@etu.univ-savoie.fr>
2024-02-20 13:55:08 +0100

    Ajoutez des fichiers projet.

---
+++ FilmCodeFirst/Models/EntityFramework/Film.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIfilms.Models.EntityFramework
+{
+    [Table("t_e_film_flm")]
+    public class Film
+    {
+        public Film()
+        {
+            NotesFilm = new HashSet<Notation>();
+        }
+
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("flm_id")]
+        public int FilmId { get; set; }
+
+        [Required]
+        [Column("flm_titre")]
+        [StringLength(100)]
+        public string Titre { get; set; } = null!;
+
+        [Column("flm_resume")]
+        public string? Resume { get; set; }
+
+        [Column("flm_datesortie", TypeName = "date")]
+        public DateTime? DateSortie { get; set; }
+
+        [Column("flm_duree", TypeName = "numeric(3, 0)")]
+        public decimal? Duree { get; set; }
+
+        [Column("flm_genre")]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [InverseProperty(nameof(Notation.FilmNote))]
+        public virtual ICollection<Notation> NotesFilm { get; set; }
+    }
+}
---
+++ FilmCodeFirst/Models/EntityFramework/Notation.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIfilms.Models.EntityFramework
+{
+    [Table("t_j_notation_not")]
+    public partial class Notation
+    {
+        [Key]
+        [Column("utl_id")]
+        public int UtilisateurId { get; set; }
+
+        [Key]
+        [Column("flm_id")]
+        public int FilmId { get; set; }
+
+        [Column("not_note")]
+        public int Note { get; set; }
+
+        [ForeignKey(nameof(FilmId))]
+        [InverseProperty(nameof(Film.NotesFilm))]
+        public virtual Film FilmNote { get; set; } = null!;
+
+        [ForeignKey(nameof(UtilisateurId))]
+        [InverseProperty(nameof(Utilisateur.NotesUtilisateur))]
+        public virtual Utilisateur UtilisateurNotant { get; set; } = null!;
+    }
+}
